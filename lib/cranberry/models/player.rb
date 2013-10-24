48af4dd7ef2c2711b4d1a5152631fb3b0beb852c
Caleb Tutty <caleb@prettymint.co.nz>
2013-10-24 23:54:50 +1300

    Added socket_id to the player class

--- lib/cranberry/models/player.rb
+++ lib/cranberry/models/player.rb
@@ -1,9 +1,12 @@
 module Cranberry
   module Models
     class Player
+      attr_reader :socket_id
       attr_accessor :name
       
       def initialize(options = {})
+        @socket_id = options[:socket_id]
+        raise "Players require a socket id to be identifiable, and valid" unless @socket_id
         @name = options[:name] || "anonymous user"
       end
 
