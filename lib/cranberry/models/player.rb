089b6d2d241cee57d3b71131708a9a7c931c4947
Caleb Tutty <caleb@prettymint.co.nz>
2013-10-27 20:30:00 +1300

    Added Pry to the development Gemfile for occassional debugging, and logic to keep track of the number of players in the world

--- lib/cranberry/models/player.rb
+++ lib/cranberry/models/player.rb
@@ -1,11 +1,11 @@
 module Cranberry
   module Models
     class Player
-      attr_reader :socket_id
+      attr_reader :socket_id, :socket
       attr_accessor :name
       
       def initialize(options = {})
-        @socket_id = options[:socket_id]
+        @socket_id, @socket = options[:socket_id], options[:socket]
         raise "Players require a socket id to be identifiable, and valid" unless @socket_id
         @name = options[:name] || "anonymous user"
       end
