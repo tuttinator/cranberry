9cec8cbfe813217c26324bb7444192e2a9741c7b
Caleb Tutty <caleb@prettymint.co.nz>
2013-10-29 21:08:23 +1300

    Moved the banner messages and fixed the reset cursor positioning

--- lib/cranberry.rb
+++ lib/cranberry.rb
@@ -7,16 +7,7 @@ require 'json'
 require 'paint'
 
 # Cranberry files
-require 'cranberry/version'
-require 'cranberry/banner'
-require 'cranberry/configuration'
-# TODO load all .rb files in handler and model dirs
-require 'cranberry/handlers/log'
-require 'cranberry/handlers/movement'
-require 'cranberry/models/world'
-require 'cranberry/models/grid'
-require 'cranberry/models/player'
-require 'cranberry/ui/console'
-require 'cranberry/websocket_server'
-require 'cranberry/server'
-require 'cranberry/router'
+Dir[File.dirname(__FILE__) + '/cranberry/**/*.rb'].each do |file|
+  require file
+end
+
