7b19a812b122195dbd207e66c9a29e185994d6d0
Caleb Tutty <caleb@prettymint.co.nz>
2013-11-02 21:12:17 +1300

    Moved EventMachine.run method into cranberry.rb

--- lib/cranberry.rb
+++ lib/cranberry.rb
@@ -6,8 +6,25 @@ require 'em-websocket'
 require 'json'
 require 'paint'
 
-# Cranberry files
+# Require all files in the Cranberry folder
 Dir[File.dirname(__FILE__) + '/cranberry/**/*.rb'].each do |file|
   require file
 end
 
+# Main entrypoint
+module Cranberry
+  def self.run
+    EventMachine.run do
+      trap("TERM") { self.stop }
+      trap("INT")  { self.stop }
+      WebSocketServer.start
+    end
+  end
+
+  def self.stop
+    UI.clear_terminal_screen
+    UI.display_termination_message
+    EventMachine.stop
+  end
+
+end
--- spec/cranberry/server_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Cranberry do
-
-  describe "server" do
-    it "works"
-  end
-
-end
