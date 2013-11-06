8120f10cb59e3b351514a1a15b97f054509339fb
Giles Thompson <giles@papaiti.com>
2013-11-06 22:49:37 +1300

    mocked player creation so that the spec passes

--- spec/cranberry/models/player_spec.rb
+++ spec/cranberry/models/player_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Cranberry::Models::Player do
 
-  let(:player) {Player.new}
+  let(:player) { Cranberry::Models::Player.new({ socket_id: 'blah', socket: 'replace with websocket object' }) }
 
   it "starts with the name 'anonymous user'" do
     expect(player.name).to eq('anonymous user')
