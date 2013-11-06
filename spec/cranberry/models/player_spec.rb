44132b9576928dd1115395933fb5ca63bf937d24
Caleb <caleb@prettymint.co.nz>
2013-11-06 14:48:36 +1300

    Started player spec

--- spec/cranberry/models/player_spec.rb
+++ spec/cranberry/models/player_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe Cranberry::Models::Player do
 
-  it "works"
+  let(:player) {Player.new}
+
+  it "starts with the name 'anonymous user'" do
+    expect(player.name).to eq('anonymous user')
+  end
 
 end
