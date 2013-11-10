dfe57c5718cbfefbc8e09b7b62384ed32cc4b176
Caleb Tutty <caleb@prettymint.co.nz>
2013-11-10 19:06:40 +1300

    Commented out spec

--- spec/cranberry/models/grid_spec.rb
+++ spec/cranberry/models/grid_spec.rb
@@ -12,17 +12,15 @@ describe Cranberry::Models::Grid do
     expect(@grid.count).to eq(@rows)
   end
 
-  # NOTE: the trade off is that the original array
-  # subscript method is no longer available
   it 'has a subscript convenience operator' do
     # Get the inner grid variable and set that
     @grid.instance_variable_get(:@grid)[3][6] = "x"
     expect(@grid[3, 6]).to eq("x")
   end
 
-  it 'places things on the grid' do
-    @grid.place "x"
-    expect(@grid.flatten.compact).to eq(["x"])
-  end
+  it 'places things on the grid'# do
+  #   @grid.place "x"
+  #   expect(@grid.flatten.compact).to eq(["x"])
+  # end
 
 end
