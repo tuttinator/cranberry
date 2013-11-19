b62370abe8275d4c84844bced7562f134a080870
Caleb Tutty <caleb@prettymint.co.nz>
2013-11-19 19:01:39 +1300

    Stubbing next test

--- spec/integration_tests/cli_spec.rb
+++ spec/integration_tests/cli_spec.rb
@@ -2,12 +2,14 @@ require 'spec_helper'
 
 describe 'the command line interface' do
 
-  it 'should boot' do
+  it 'boot' do
     expect do
       Thread.new do
         Cranberry.run
       end
     end.to_not raise_error
   end
+
+  it 'listens for websocket connections'
 
 end
